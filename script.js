cb0b3aef66b7c0c0e7c57ca3f4abb9853320b653
Zaid Loni <96578621+zaidloni@users.noreply.github.com>
2023-01-11 14:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your code here
+let textArea = document.getElementById("evaluatedText");
+let h3 = document.getElementById("letterCount");
+
+textArea.addEventListener("keyup", () => {
+  func();
+});
+
+let func = () => {
+  console.log("helli");
+  let count = textArea.value.length;
+  h3.innerText = count;
+};
